9b066a0492670b1160cb926a882d69f583c93b95
Kimaru <george.kimaru98@gmail.com>
2022-11-11 12:36:59 +0300

    File rename

--- trial.py
+++ trial.py
@@ -1,50 +1,64 @@
-from sympy import *
 import random
 
 
-def write_numbers():
- with open('test.txt', 'w') as file:
-  x = int(input("How many numbers are needed to write to the file: "))
-  for i in range(x):
-   random.seed()
-   nums = random.randint(1, 100)
-   print(nums, end=" ")
-   # print(nums)
-   file.write(str(nums) + " ")
+def isPrime(n):
+    for i in range(2, n):
+        if (n % i) == 0:
+            return False
+    else:
+        return True
 
- return nums
-
-def prime_numbers():
- nums = write_numbers()
- print(type(nums))
- print(nums)
- with open('test.txt', 'r') as input:
-  with open('prime.txt', 'w') as output:
-   for line in input:
-    output.write(line)
- 
-
-# write_numbers()
-prime_numbers()
-
- # my_list=[]
- # my_file = open("test.txt", 'rt')
- # content = my_file.read()
- # my_file.close()
- # content = content.split()
-
- 
-
- # my_list.append(content)
- # print(type(my_list))
- # print(my_list[1])
- # for i in my_list:
- #  print(i)
- #  if isprime(i) == True:
- #   print(i)
- #   with open('prime.txt', 'w') as file:
- #    file.write(str(i) + " ")
+# sum of digits of a number
+def sumOfDigit(n):
+    s = 0
+    while n > 0:
+        d = n % 10
+        n = n // 10
+        s += d
+    return s
 
 
+def assignment():
+    f = open("test.txt", "w")
+    prime = open("prime.txt", "w")
+    n = int(input("How many numbers are needed to write to the file: "))
+    # Main lab: random.seed(1)
+    # Assignment 1: random.seed(2)
+    random.seed(2)
+    maxSumOfDigit = -1
+    num=0
+    for _ in range(n):
+        # random number between 1 and 100
+        r = random.randint(1, 100)
+        f.write(str(r) + "\n")
+        if isPrime(r):
+            prime.write(str(r) + "\n")
+        if sumOfDigit(r) > maxSumOfDigit:
+            maxSumOfDigit = sumOfDigit(r)
+            num=r
+
+    f.close()
+    prime.close()
+
+    # read file and output to console
+
+    f = open("test.txt", "r")
+    prime = open("prime.txt", "r")
+
+    print("All numbers in the file: ")
+    for i in f:
+        print(i.strip(), end=" ")
+
+    print("\nThe list of prime numbers: ")
+    countPrime = 0
+    for i in prime:
+        countPrime += 1
+        print(i.strip(), end=" ")
+
+    print("\n%d prime numbers found in this file" % countPrime)
+    print("%d has a maximum sum of digits = %d" % (num, maxSumOfDigit))
+    f.close()
+    prime.close()
 
 
+assignment()
